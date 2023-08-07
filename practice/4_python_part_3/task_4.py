9338d82f57e6ba63977caabeb65c84f056b8ee5b
Batalia123 <nwykpis@griddynamics.com>
2023-08-07 19:35:44 +0200

    task and tests done

--- practice/4_python_part_3/task_4.py
+++ practice/4_python_part_3/task_4.py
@@ -17,9 +17,34 @@ Example:
 import argparse
 
 
-def print_name_address(args: argparse.Namespace) -> None:
-    ...
+import argparse
+from faker import Faker
+import json
 
+def generate_dicts(number, fields):
+    fake = Faker()
+    dicts = []
+    for _ in range(number):
+        data = {}
+        for field in fields:
+            key, provider = field.split('=')
+            data[key] = getattr(fake, provider)()
+        dicts.append(data)
+    return dicts
+
+def print_name_address(args: argparse.Namespace) -> None:
+    number = int(args.number)
+    fields = args.fields
+    dicts = generate_dicts(number, fields)
+    for data in dicts:
+        print(json.dumps(data))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate dicts with fake data.")
+    parser.add_argument("number", help="Number of generated instances", type=int)
+    parser.add_argument("--fields", nargs='+', help="Key and provider pairs", required=True)
+    args = parser.parse_args()
+    print_name_address(args)
 
 """
 Write test for print_name_address function
@@ -30,3 +55,29 @@ Example:
     >>> m.method()
     123
 """
+
+
+import unittest
+from unittest.mock import Mock, patch
+
+class TestPrintNameAddress(unittest.TestCase):
+
+    @patch('builtins.print')
+    def test_print_name_address(self, mock_print):
+        mock_args = Mock()
+        mock_args.number = 2
+        mock_args.fields = ['fake-address=address', 'some_name=name']
+
+        print_name_address(mock_args)
+
+        actual_calls = mock_print.call_args_list
+
+        for actual_call in actual_calls:
+            call_args, _ = actual_call
+            generated_dict_str = call_args[0]
+            generated_dict = eval(generated_dict_str)
+            self.assertTrue("some_name" in generated_dict)
+            self.assertTrue("fake-address" in generated_dict)
+
+if __name__ == '__main__':
+    unittest.main()
