34adb05f534d937922e3e374422f20698d53827d
Batalia123 <nwykpis@griddynamics.com>
2023-08-07 20:07:49 +0200

    task and tests done

--- practice/4_python_part_3/task_5.py
+++ practice/4_python_part_3/task_5.py
@@ -2,14 +2,27 @@
 Write a function that makes a request to some url
 using urllib. Return status code and decoded response data in utf-8
 Examples:
-     >>> make_request('https://www.google.com')
+     # >>> make_request('https://www.google.com')
      200, 'response data'
 """
 from typing import Tuple
-
+from urllib import request
 
 def make_request(url: str) -> Tuple[int, str]:
-    ...
+    try:
+        response = request.urlopen(url)
+        status_code = response.getcode()
+        response_data = response.read().decode('utf-8')
+        return status_code, response_data
+    except Exception as e:
+        return -1, str(e)
+
+# Example usage
+if __name__ == "__main__":
+    url = 'https://www.google.com'
+    status_code, response_data = make_request(url)
+    print(status_code)
+    print(response_data[:200])
 
 
 """
@@ -22,5 +35,28 @@ Example:
     >>> m.method()
     200
     >>> m.method2()
-    b'some text'
+    'some text'
 """
+
+import unittest
+from unittest.mock import Mock, patch
+
+class TestMakeRequest(unittest.TestCase):
+
+    @patch('urllib.request.urlopen')
+    def test_make_request(self, mock_urlopen):
+        # Simulate a successful response
+        mock_response = Mock()
+        mock_response.getcode.return_value = 200
+        mock_response.read.return_value = b'response data'
+        mock_urlopen.return_value = mock_response
+
+        # Call the function with the mocked urlopen
+        status_code, response_data = make_request('https://www.example.com')
+
+        # Verify the output
+        self.assertEqual(status_code, 200)
+        self.assertEqual(response_data, 'response data')
+
+if __name__ == '__main__':
+    unittest.main()
