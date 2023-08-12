a74e1327c087d4a2dccc0da2f9b0491d1a99b33b
Batalia123 <nwykpis@griddynamics.com>
2023-08-12 02:44:57 +0200

    tests done

--- practice/3_python_testing/test_task_input_output.py
+++ practice/3_python_testing/test_task_input_output.py
@@ -11,12 +11,25 @@ https://docs.python.org/3/library/unittest.mock.html#unittest.mock.patch
 
 TIP: for testing builtin input() function create another function which return input() and mock returned value
 """
+import unittest
 from unittest.mock import patch
+import sys
+sys.path.append('/Users/nwykpis/PYTHON-BASIC/practice/2_python_part_2')
+
+from task_input_output import *
 
 
-def test_read_numbers_without_text_input():
-    ...
+class TestReadNumbers(unittest.TestCase):
 
+    @patch('builtins.input', side_effect=['1', '2', 'hello', '2', 'world'])
+    def test_read_numbers_with_mixed_input(self, mock_input):
+        result = read_numbers(5)
+        self.assertEqual(result, "Avg: 1.67")
 
-def test_read_numbers_with_text_input():
-    ...
+    @patch('builtins.input', side_effect=['hello', 'world', 'foo', 'bar', 'baz'])
+    def test_read_numbers_with_no_numbers(self, mock_input):
+        result = read_numbers(5)
+        self.assertEqual(result, "No Numbers entered")
+
+if __name__ == '__main__':
+    unittest.main()
