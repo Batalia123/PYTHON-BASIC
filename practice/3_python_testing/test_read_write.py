ff1b1f6168d51e45d1be45fb2ce4b51a0c543f1d
Batalia123 <nwykpis@griddynamics.com>
2023-08-13 19:57:48 +0200

    test done

--- practice/3_python_testing/test_read_write.py
+++ practice/3_python_testing/test_read_write.py
@@ -4,3 +4,20 @@ To write files during tests use temporary files:
 https://docs.python.org/3/library/tempfile.html
 https://docs.pytest.org/en/6.2.x/tmpdir.html
 """
+import sys
+import random
+import tempfile
+sys.path.append('/Users/nwykpis/PYTHON-BASIC/practice/2_python_part_2')
+from task_read_write import *
+
+def test_read_write():
+    numbers = [str(random.randint(0, 100)) for _ in range(20)]
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        for i, number in enumerate(numbers, start=1):
+            with open(f"{tmpdirname}/file_{i}.txt", 'w') as f:
+                f.write(number)
+
+        values = read_from_file(tmpdirname, 'file_')
+        write_to_file(values, f'{tmpdirname}/result.txt')
+        with open(f"{tmpdirname}/result.txt", 'r') as f:
+            assert f.read().split(', ') == numbers
