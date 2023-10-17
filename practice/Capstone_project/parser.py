56a9f65365e4a1f2744e66cc4cf1607034ce173c
Batalia123 <nwykpis@griddynamics.com>
2023-10-17 03:54:59 +0200

    all done without tests

--- practice/Capstone_project/parser.py
+++ practice/Capstone_project/parser.py
@@ -1,54 +1,36 @@
 import argparse
 import json
-import re
-import uuid
-import random
-import time
 import os
 import multiprocessing
+import configparser
+import logging
+import random
+import time
+import uuid
+import re
 
-parser = argparse.ArgumentParser(prog='test data generator',
-                                 description='generate test data based on the provided schema')
-parser.add_argument('--data_schema', type=str, help='pass dataschema guidelines or location')
-parser.add_argument('--data_lines', default=1, type=int, help='pass number of lines in each file')
-parser.add_argument('--file_count', default=0, type=int, help='pass number of output files')
-parser.add_argument('--file_name', default='file_number_', type=str, help='pass template output file name')
-parser.add_argument('--file_prefix', type=str, choices=['count', 'random', 'uuid'],
-                    help='pass file prefix when more than one file')
-parser.add_argument('--path_to_save_files', default='./output/', type=str, help='pass path to save output files')
-parser.add_argument('--clear_path', action='store_true')
-parser.add_argument('--multiprocessing', type=int, default=1, help='pass number of processes used to create files')
+logging.basicConfig(filename='logging3.txt', level=logging.INFO)
 
-# data_schema="{\"date\": \"timestamp:\",\"name\": \"str:rand\",\"type\": \"str:['client', 'partner', 'government']\",\"age\": \"int:rand(1, 90)\"}"
-# args = json.dumps(test)
-args = parser.parse_args().data_schema  # Pass the JSON string as an argument
+def parse_arguments():
+    parser = argparse.ArgumentParser(prog='test data generator', description='generate test data based on the provided schema')
+    parser.add_argument('--data_schema', type=str, dest='data_schema', help='pass dataschema guidelines or location')
+    parser.add_argument('--data_lines', dest='data_lines', type=int, help='pass number of lines in each file')
+    parser.add_argument('--file_count', dest='file_count', type=int, help='pass number of output files')
+    parser.add_argument('--file_name', dest='file_name', type=str, help='pass template output file name')
+    parser.add_argument('--file_prefix', type=str, choices=['count', 'random', 'uuid'], dest='file_prefix', help='pass file prefix when more than one file')
+    parser.add_argument('--path_to_save_files', dest='path_to_save_files', type=str, help='pass path to save output files')
+    parser.add_argument('--clear_path', action='store_true')
+    parser.add_argument('--multiprocessing', type=int, dest='multiprocessing', help='pass number of processes used to create files')
 
-if args[-4:] == 'json':
-    with open(args) as json_file:
-        args_dict = json.load(json_file)
-        print(args_dict)
-else:
-    print(False)
-    args_dict = json.loads(args)
+    return parser.parse_args()
 
+def load_schema(data_schema_from_input):
+    if data_schema_from_input[-4:] == 'json':
+        with open(data_schema_from_input) as json_file:
+            return json.load(json_file)
+    return json.loads(data_schema_from_input)
 
-def create_file(path_to_save_files, file_name, line_count, prefix, start, end):
-    for i in range(start, end):
-        if prefix == 'count':
-            completeName = os.path.join(path_to_save_files, file_name + str(file + 1) + ".json")
-        elif prefix == 'uuid':
-            completeName = os.path.join(path_to_save_files, file_name + str(uuid.uuid4()) + ".json")
-        elif prefix == 'random':
-            completeName = os.path.join(path_to_save_files, file_name + str(random.randint(1, 1000)) + ".json")
-        with open(completeName, "w") as f:
-            print(completeName)
-            for line in range(line_count):
-                content = json.dumps(generate_single_record(), indent=4)
-                f.write(content + '\n')
-                print(content)
-
-
-def generate_single_record():
+def generate_single_record(args_dict):
     generated_test = dict()
     for key, value in args_dict.items():
         type_list = value.split(':')
@@ -57,7 +39,8 @@ def generate_single_record():
             if type_list[1] == '':
                 generated_test[key] = ''
             elif type_list[1][:5] == 'rand(':
-                generated_test[key] = 'error'
+                logging.error("random value within bounds can't be string, setting it to empty string")
+                generated_test[key] = ''
             elif type_list[1] == 'rand':
                 generated_test[key] = str(uuid.uuid4())
             elif type_list[1][0] == '[':
@@ -81,42 +64,91 @@ def generate_single_record():
                 generated_test[key] = int(type_list[1])
         elif type_list[0] == 'timestamp':
             generated_test[key] = time.time()
+        
     return generated_test
 
+def create_file(path_to_save_files, file_name, line_count, prefix, start, end, args_dict):
+    for file in range(start, end):
+        if prefix == 'count':
+            complete_name = os.path.join(path_to_save_files, f"{file_name}{file + 1}.json")
+        elif prefix == 'uuid':
+            complete_name = os.path.join(path_to_save_files, f"{file_name}{uuid.uuid4()}.json")
+        elif prefix == 'random':
+            complete_name = os.path.join(path_to_save_files, f"{file_name}{random.randint(1, 1000)}.json")
 
-def generate_records():
-    prefix: object = parser.parse_args().file_prefix
-    file_count = parser.parse_args().file_count
-    path_to_save_files = parser.parse_args().path_to_save_files
-    file_name = parser.parse_args().file_name
-    process_count = parser.parse_args().multiprocessing
-    files_per_process = file_count // process_count
+        logging.info(f"Creating file: {complete_name}")
 
+        with open(complete_name, "w") as f:
+            for _ in range(int(line_count)):
+                content = json.dumps(generate_single_record(args_dict), indent=4)
+                f.write(content + '\n')
+                print(content)
+
+        logging.info(f"File {complete_name} created successfully")
+
+def clear_output_directory(path_to_save_files, file_name):
     files = os.listdir(path_to_save_files)
     for file in files:
         if file.startswith(file_name):
             file_path = os.path.join(path_to_save_files, file)
             os.remove(file_path)
 
-    for file in range(file_count):
-        line_count = parser.parse_args().data_lines
+def generate_records(args):
+    logging.info('Starting to get the parameters')
+    prefix = args['file_prefix']
+    file_count = max(0, int(args['file_count']))
+    path_to_save_files = args['path_to_save_files']
+    file_name = args['file_name']
+    process_count = max(1, int(args['multiprocessing']))
+    line_count = int(args['data_lines'])
+    files_per_process = file_count // process_count
+    logging.info('Parameters fetched')
+
+    if args['clear_path']:
+        logging.info("Starting to clear the output directory")
+        clear_output_directory(path_to_save_files, file_name)
+        logging.info("Output directory cleared")
+
+    logging.info(f"Number of files is {file_count}")
+    if file_count == 0:
+        logging.info("Writing output to terminal")
+        for _ in range(int(line_count)):
+            content = json.dumps(generate_single_record(args_dict), indent=4)
+            print(content)
+        logging.info("Output generated")
+        return
+
+    logging.info(f"Number of processes to create the output files is {process_count}")
 
     processes = []
     for i in range(process_count):
+        logging.info("Starting to create files")
         start = i * files_per_process
         end = (i + 1) * files_per_process if i < file_count - 1 else file_count
-        process = multiprocessing.Process(target=create_file, args=(path_to_save_files, file_name, line_count, prefix, start, end))
+
+        process = multiprocessing.Process(target=create_file,
+                                          args=(path_to_save_files, file_name, line_count, prefix, start, end, args_dict))
         processes.append(process)
         process.start()
 
     for process in processes:
         process.join()
 
+    logging.info("Output generated")
 
 if __name__ == '__main__':
-    generate_records()
+    config = configparser.ConfigParser()
+    config.read('default.ini')
+    defaults = config['default']
 
-# for key, value in args.items():
+    args_from_input = vars(parse_arguments())
+    args = dict(defaults)
+    args.update({k: v for k, v in args_from_input.items() if v is not None})
 
+    data_schema_from_input = args['data_schema']  # Pass the JSON string as an argument
+    args_dict = load_schema(data_schema_from_input)
 
-# print(args.data_schema)
+    for k, v in args.items():
+        print(f"{k} : {v}")
+
+    generate_records(args)
